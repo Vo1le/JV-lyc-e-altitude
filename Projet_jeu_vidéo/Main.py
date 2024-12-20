64ce7bca3f8d098beeca8a59eb43a0a0faf35d63
elliott005 <elliott.castellazzi@gmail.com>
2024-12-20 18:45:26 +0100

    ajouté preview de sauvegardes + supprimer sauvegardes

--- Projet_jeu_vidéo/Main.py
+++ Projet_jeu_vidéo/Main.py
@@ -2,7 +2,7 @@
 import pygame as py
 from pygame.locals import * 
 import sys
-import os.path
+import os, os.path
 import pickle
 from maps.attributs import GAME_SCREEN_WIDTH, GAME_SCREEN_HEIGHT
 
@@ -53,13 +53,13 @@ def main():
         keys_pressed_once = []
         for event in py.event.get():
             if event.type == py.QUIT:
-                saveGame(id, location, joueur)
+                saveGame(id, location, joueur, ecran)
                 py.quit()
                 sys.exit()
             if event.type == py.KEYDOWN:
                 keys_pressed_once.append(event.key)
                 if verifierInputKey(event.key, "quit"):
-                    saveGame(id, location, joueur)
+                    saveGame(id, location, joueur, ecran)
                     py.quit()
                     sys.exit()
                 elif verifierInputKey(event.key, "zoom"):
@@ -113,30 +113,71 @@ def main():
 # // fonction diverses //
 
 def mainMenu():
-    saveSlots = [{"rect": py.Rect(50, i * 150 + 25, 200, 125), "id": i} for i in range(NUM_SAVE_SLOTS)]
+    deleteImageSize = (75, 75)
+    deleteImage = py.transform.scale(py.image.load("images/Shuriken.png"), deleteImageSize)
+    deleting = -1
+    deleteText = ""
+    deleteButtons = [py.Rect((260, i * 150 + 50), deleteImageSize) if os.path.isfile(SAVE_FOLDER + "save" + str(i) + ".txt") else False for i in range(NUM_SAVE_SLOTS)]
+
+    saveSlots = [{"rect": py.Rect(50, i * 150 + 25, 200, 125), "image": False if not os.path.isfile(SAVE_FOLDER + "save" + str(i) + ".png") else py.image.load(SAVE_FOLDER + "save" + str(i) + ".png")} for i in range(NUM_SAVE_SLOTS)]
     running = True
+    hovered = 0
     while running:
+        mouse_x, mouse_y = py.mouse.get_pos()
+        for i, button in enumerate(saveSlots):
+            if button["rect"].collidepoint((mouse_x, mouse_y)):
+                hovered = i
         for event in py.event.get():
             if event.type == py.KEYDOWN:
                 if event.key == py.K_ESCAPE:
                     py.quit()
                     sys.exit()
-            if event.type == py.QUIT:
+                elif event.key != pygame.K_RETURN and deleting != -1:
+                    if event.key == pygame.K_BACKSPACE:
+                        deleteText = deleteText[:-1]
+                    else:
+                        deleteText += event.unicode
+                elif event.key == pygame.K_RETURN and deleting != -1:
+                    if deleteText.lower() == "oui":
+                        deleteButtons[deleting] = False
+                        saveSlots[deleting]["image"] = False
+                        fileName = SAVE_FOLDER + "save" + str(deleting)
+                        os.remove(fileName + ".txt")
+                        os.remove(fileName + ".png")
+                    deleting = -1
+                    deleteText = ""
+            elif event.type == py.QUIT:
                 py.quit()
                 sys.exit()
-            if event.type == py.MOUSEBUTTONDOWN:
+            elif event.type == py.MOUSEBUTTONDOWN:
                 if event.button == 1:
-                    mouse_x, mouse_y = py.mouse.get_pos()
-                    for button in saveSlots:
+                    for i, button in enumerate(saveSlots):
                         if button["rect"].collidepoint((mouse_x, mouse_y)):
-                            return loadSave(button["id"])
+                            return loadSave(i)
+                    for i, button in enumerate(deleteButtons):
+                        if button and button.collidepoint((mouse_x, mouse_y)):
+                            deleting = i
         
         ecran.fill(color)
 
-        for button in saveSlots:
-            py.draw.rect(ecran, (100, 200, 0), button["rect"], 10)
-            text_img = FONT.render("sauvegarde: " + str(button["id"]), True, (0, 100, 100))
+        for i, button in enumerate(saveSlots):
+            rectColor = (100, 200, 0) if i != hovered else (0, 100, 200)
+            py.draw.rect(ecran, rectColor, button["rect"], 10)
+            text_img = FONT.render("sauvegarde: " + str(i), True, (0, 100, 100))
             ecran.blit(text_img, (button["rect"].topleft[0] + 20, button["rect"].topleft[1] + 50))
+            if i == hovered and button["image"]:
+                ecran.blit(button["image"], (GAME_SCREEN_WIDTH - 825, 50))
+        
+        for button in deleteButtons:
+            if button:
+                ecran.blit(deleteImage, button.topleft)
+
+        if deleting != -1:
+            py.draw.rect(ecran, (200, 200, 200), (40, 40, GAME_SCREEN_WIDTH / 1.5, GAME_SCREEN_HEIGHT / 1.5))
+            text_img = FONT.render("confirmez que vous voulez supprimer la sauvegarde: " + str(deleting) + " (oui/non)", True, (0, 100, 100))
+            ecran.blit(text_img, (60, 60))
+            text_img = FONT.render(deleteText, True, (0, 100, 100))
+            ecran.blit(text_img, (75, 120))
 
         pygame.display.update()
 
@@ -151,9 +192,10 @@ def loadSave(id=0):
     else:
         return id, {"destination": "overworld", "position": (100, 125)}, Joueur(100, 125, {}, {})
 
-def saveGame(id, location, joueur: Joueur):
-    fileName = SAVE_FOLDER + "save" + str(id) + ".txt"
-    with open(fileName, "wb") as f:
+def saveGame(id, location, joueur: Joueur, preview: py.Surface):
+    fileName = SAVE_FOLDER + "save" + str(id)
+    py.image.save(py.transform.scale(preview, (800, 448)), fileName + ".png")
+    with open(fileName + ".txt", "wb") as f:
         pickle.dump({"location": location, "items": joueur.items, "collectedItems": joueur.collectedItems, "position": joueur.rect.center, "events": joueur.evenements}, f)
 
 if __name__ == "__main__":
